957d584f238a902f4e45853257f6004f45b7e59e
Surajjj0030 <surajtawade0068@gmail.com>
2025-04-14 15:21:26 +0530

    model_eval

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from src.cnnClassifier.utils.common import read_yaml,create_directories
 from src.cnnClassifier.constants import *
-from src.cnnClassifier.entity.config import DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig
+from src.cnnClassifier.entity.config import DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig,EvaluationConfig
 import os
 
 class ConfigurationManager:
@@ -69,3 +69,15 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/kidney-ct-scan-image",
+            mlflow_uri="https://dagshub.com/Surajjj0030/AI_DL-project.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+    
